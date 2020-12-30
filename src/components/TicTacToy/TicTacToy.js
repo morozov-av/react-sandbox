04b289adff734267941c6012d3c41c774f08a9e1
Yauhen-Yarash <bokakhado@mail.com>
2020-12-30 21:53:55 +0300

    TicTacToy update

--- src/components/TicTacToy/TicTacToy.js
+++ src/components/TicTacToy/TicTacToy.js
@@ -2,32 +2,104 @@ import React from 'react';
 import './styles/TicTacToy.scss';
 import { ReactSVG } from 'react-svg'
 
+const players = {
+  o: {
+    className: "circle",
+    fileName: "circle.svg"
+  },
+  x: {
+    className: "cross",
+    fileName: "cross.svg"
+  },
+  firstPlayer: "o",
+  secondPlayer: "x"
+};
+
+const reducer = (action) => (state, props) => {
+  switch (action.type) {
+    case "FILL_CELL": {
+      console.log("FILL_CELL");
+      let resultState = {...state};
+      resultState.field[action.key].value = action.value;
+      return resultState;
+    }
+    case "CHANGE_CURRENT_PLAYER": {
+      console.log("CHANGE_CURRENT_PLAYER");
+      return Object.assign({}, state, {
+        currentPlayer: action.playerCode
+      });
+    }
+    default:
+      return state
+  }
+};
+
 class TicTacToy extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        fieldIsReady: true,
+        currentPlayer: "x",
+        firstPlayer: null,
+        field: {
+                  "0_0": {value: null, onClick: this.makeTableCellOnClick("0_0")},
+                  "0_1": {value: "o", onClick: this.makeTableCellOnClick("0_1")},
+                  "0_2": {value: null, onClick: this.makeTableCellOnClick("0_2")},
+                  "1_0": {value: null, onClick: this.makeTableCellOnClick("1_0")},
+                  "1_1": {value: null, onClick: this.makeTableCellOnClick("1_1")},
+                  "1_2": {value: "x", onClick: this.makeTableCellOnClick("1_2")},
+                  "2_0": {value: null, onClick: this.makeTableCellOnClick("2_0")},
+                  "2_1": {value: null, onClick: this.makeTableCellOnClick("2_1")},
+                  "2_2": {value: null, onClick: this.makeTableCellOnClick("2_2")}
+                }
+      }
+  }
+
   render() {
     return(
       <div className="tictac-container">
         <ReactSVG className="lattice" src="lattice.svg"/>
-        <table>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-            </tr>
-            <tr>
-              <td>4</td>
-              <td>5</td>
-              <td>6</td>
-            </tr>
-            <tr>
-              <td>7</td>
-              <td>8</td>
-              <td>9</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="field">
+            {this.renderTable()}
+        </div>
       </div>
     );
+  }
+
+  renderTable() {
+    return Object.entries(this.state.field).map(([key, value], i) =>
+      <div key={i + 1} className="field-cell" onClick={value.onClick}>{this.renderTableCell(value)}</div>
+    )
+  }
+
+  renderTableCell(value) {
+    if(value.value) {
+      let player = players[value.value];
+      return <ReactSVG className={player.className} src={player.fileName}/>;
+    } else {
+      return "";
+    }
+  }
+
+  makeTableCellOnClick(key) {
+    return () => {
+      if (!this.state.field[key].value) {
+        console.log(key);
+        this.setState(reducer({
+          type: "FILL_CELL",
+          key: key,
+          value: this.state.currentPlayer
+        }));
+        this.setState(reducer({
+            type: "CHANGE_CURRENT_PLAYER",
+            playerCode: this.nextPlayerCode()
+        }))
+      }
+    }
+  }
+
+  nextPlayerCode() {
+      return this.state.currentPlayer == players.firstPlayer ? players.secondPlayer : players.firstPlayer
   }
 }
 
